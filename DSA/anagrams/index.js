24fb49565f1f7b7a1cb86c885f4b9448d5e66027
23pras <palprashant465@gmail.com>
2021-12-30 17:20:00 +0530

    JS created

---
+++ DSA/anagrams/index.js
@@ -0,0 +1,49 @@
+// Check if two strings are anagrams of each other.
+// One string is an anagram of another if it uses exact same characters
+// in exact same quantity. Only consider word characters, and make sure the
+// function is case insensitive.
+// --- Examples
+//   anagrams('heart', 'earth') --> True
+//   anagrams('heart', '  earth') --> True
+//   anagrams('Heart!', 'EARTH') --> True
+//   anagrams('lol', 'lolc') --> False
+
+function anagrams(stringA, stringB) {
+  return "yes"
+}
+
+
+
+
+
+
+
+
+
+
+mocha.setup("bdd");
+const { assert } = chai;
+
+describe("Anagrams", () => {
+  it("works if case sensitivity and non word characters NOT taken into account", () => {
+    assert.equal(anagrams("earth", "heart"), true);
+
+    assert.equal(anagrams("love", "meow"), false);
+    assert.equal(anagrams("lol", "lolc"), false);
+  });
+  it("is case insensitive. 'HEART' and 'earth' should return true", () => {
+    assert.equal(anagrams("HEART", "earth"), true);
+    assert.equal(anagrams("heart", "EARTH"), true);
+
+    assert.equal(anagrams("love", "meow"), false);
+    assert.equal(anagrams("lol", "lolc"), false);
+  });
+  it("only matches word characters. 'heart!'' and '' earth' should return true", () => {
+    assert.equal(anagrams("heart!", " earth"), true);
+
+    assert.equal(anagrams("love", "meow"), false);
+    assert.equal(anagrams("lol", "lolc"), false);
+  });
+});
+
+mocha.run();
