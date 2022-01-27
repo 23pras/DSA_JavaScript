06ff30deb05469397a839e1138ae613679561b87
23pras <palprashant465@gmail.com>
2022-01-27 15:44:26 +0530

    array method

---
+++ JS/array.js
@@ -0,0 +1,31 @@
+let arra = [1,2,3, 'one', {name: 'Jack', age: 30}]
+
+
+// console.log(arra[4]);
+
+console.log(arra[4].name);
+
+arra[4].age = 31;
+console.log(arra);
+
+
+
+
+let arr = [];
+
+console.log(arr.length);
+
+arr.push(1);
+arr.push(2);
+arr.push(3);
+
+console.log(arr);
+
+// arr.pop(); it will remove last value, 
+arr.shift(); // it will remove first value
+
+// push() to add from last, unshift() to add from first.
+
+console.log(arr);
+
+
