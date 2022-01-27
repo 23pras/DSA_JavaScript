a08ccc39076e9e01b4318b4517b9bdf9189f93ad
23pras <palprashant465@gmail.com>
2022-01-27 15:47:35 +0530

    hoisting added

---
+++ JS/hoisting.js
@@ -0,0 +1,15 @@
+// a:[undefined] , 
+// sampleFun(){console.log('Hello World!!!');}
+
+
+
+
+console.log(a); // a = undefined
+
+var a = 10; // a = 10
+
+sampleFun2();
+
+function sampleFun() {
+  console.log('Hello World!!!');
+}
