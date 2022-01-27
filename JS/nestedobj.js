d168bb07efb64e20801a25e327cc20b84c5d23a4
23pras <palprashant465@gmail.com>
2022-01-27 15:51:55 +0530

    nested added

---
+++ JS/nestedobj.js
@@ -0,0 +1,19 @@
+let obj1 = {
+  obj2: {
+    obj3: {
+      value: 'HI'
+    }
+  }
+}
+
+obj1.obj2.obj3.value = 'Bye';
+
+console.log(obj1.obj2.obj3.value);
+
+let objSSS = {
+  name: 'Jack'
+}
+
+objSSS.name = 'John';
+
+console.log(objSSS);
