5906688a4ac277b7ace05334a5c5e0ee4c4099c9
23pras <palprashant465@gmail.com>
2022-01-27 15:12:38 +0530

    switch added

---
+++ JS/switch.js
@@ -0,0 +1,18 @@
+let char = 'x';
+
+switch(char){
+  case  1 :
+  case  'a':
+  case  'A' :
+  console.log("alpha");
+  break ;
+
+  case  2 :
+  case  'b':
+  case  'B' :
+  console.log("beta");
+  break ;
+  
+  default:
+  console.log("Null")
+}
